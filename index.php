ce39ba22d61c5cd54d6d7dea9bbb23c75205b125
satoko-k <80142169+satoko-k@users.noreply.github.com>
2021-06-23 06:33:39 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,124 +1,129 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        
-        
-        <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="<?php echo get_template_directory_uri(); ?>/css/styles.css" rel="stylesheet" />
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SATOKO</title>
+    <script
+      src="https://kit.fontawesome.com/e8e62ccbd7.js"
+      crossorigin="anonymous"
+    ></script>
 
-        <?php wp_head(); ?>
-    </head>
-    <body>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="<?php echo home_url(); ?>"><?php bloginfo("name"); ?></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../../../../index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Works</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
-                    </ul>
-                </div>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;500;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" />
+    <script src="<?php echo get_template_directory_uri(); ?>/js/script.js" defer></script>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header id="header">
+      <h1>SATOKO ISHIDA</h1>
+
+      <nav>
+        <ul>
+          <li><a href="#firstView">HOME</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#works">Works</a></li>
+
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    　
+    <div id="firstView">
+      <div class="textAnimation">SATOKO TEST</div>
+      <div class="textAnimation subtitle">welcome to my portfolio!</div>
+    </div>
+
+    <main class="">
+      <section class="about" id="about">
+        <div class="animationTarget inner">
+          <h2>About Me</h2>
+          <div class="about-box">
+            <img
+              src="<?php echo get_template_directory_uri(); ?>/images/mother.jpg"
+              class="about-img"
+              alt="私のイメージ"
+            />
+            <div class="about-info">
+              <h3>SATOKO TEST</h3>
+              <p>
+                ここにテキストここにテキストここにテキストここにテキストここにテキストここにテキストここにテキストここにテキスト
+              </p>
+              <p>
+              ここにテキストここにテキストここにテキストここにテキストここにテキストここにテキストここにテキストここにテキスト
+              </p>
+              <p class="more"><a href="">and more・・準備中</a></p>
             </div>
-        </nav>
-        <!-- Page Header-->
-        <header class="masthead" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/woks_bg.jpg')">
-            <div class="container position-relative px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <div class="site-heading">
-                            <h1>Works</h1>
-                            <span class="subheading">My works and practice</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- Main Content-->
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
-           
-                    
-                    <?php while(have_posts()): the_post(); ?>
-                        <!-- Post preview-->
-                        <div class="post-preview">
-                            <a href="<?php the_permalink(); ?>">
-                                <h2 class="post-title"><?php the_title(); ?></h2>
-                                <h3 class="post-subtitle"><?php the_excerpt(); ?></h3>
-                            </a>
-                            <p class="post-meta">
-                                Posted by
-                                <?php the_author(); ?>
-                                on <?php the_time("Y-m-d"); ?>
-                            </p>
-                        </div>
-                        <!-- Divider-->
-                        <hr class="my-4" />
-                    <?php endwhile; ?>
-                    
-                    <!-- Pager-->
-                    <div class="d-flex justify-content-end mb-4">
-                    <?php echo paginate_links(); ?>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-        <!-- Footer-->
-        <footer class="border-top">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <ul class="list-inline text-center">
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="small text-center text-muted fst-italic">Copyright &copy; Your Website 2021</div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
+      </section>
 
-        <?php wp_footer(); ?>
-    </body>
+      <section class="works" id="works">
+        <div class="animationTarget inner">
+          <h2>Works</h2>
+
+          <div class="workList">
+            <a href="" class="workItem">
+              <img src="<?php echo get_template_directory_uri(); ?>/images/zassou.png" class="work-img" alt="..." />
+              <p class="work-title">ここにテキスト</p>
+              <p class="work-info">
+              ここにテキストここにテキストここにテキストここにテキスト
+              </p>
+            </a>
+
+            <a href="" class="workItem">
+              <img src="<?php echo get_template_directory_uri(); ?>/images/zukan.png" class="work-img" alt="..." />
+              <p class="work-title">ここにテキスト</p>
+              <p class="work-info">ここにテキストここにテキストここにテキスト。
+              </p>
+            </a>
+
+            <a href="./works/works03/index.html" class="workItem">
+              <img src="<?php echo get_template_directory_uri(); ?>/images/work01.jpg" class="work-img" alt="..." />
+              <p class="work-title">ここにテキスト</p>
+              <p class="work-info">
+              ここにテキストここにテキストここにテキストここにテキスト。
+              </p>
+            </a>
+
+          </div>
+          <p class="more">
+            <a href="">and more 準備中>></a>
+          </p>
+        </div>
+      </section>
+
+      <section class="contact" id="contact">
+        <div class="animationTarget inner">
+          <h2>Contact</h2>
+          <p>下記よりお気軽にお問い合わせください。</p>
+          <p class="btn_to_form">
+            <a href="./wp/contact/">
+              <i class="fa fa-envelope-o"></i> お問い合わせフォーム</a
+            >
+          </p>
+        </div>
+      </section>
+    </main>
+
+    <p class="pagetop">
+      <a id="pagetop"><i class="fa fa-chevron-up"></i></a>　
+    </p>
+    <!-- footer -->
+    <footer class="footer">
+      <div class="copyright">&copy;SATOKO ISHIDA</div>
+    </footer>
+    <!-- /footer -->
+    <!-- jQueryを読み込む！必ず先に！ -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+    <?php wp_footer(); ?>
+  </body>
 </html>
